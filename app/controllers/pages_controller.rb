f5be83f032ef37b65aaed4a711a70ae3796774ec
florian lahitte <lahitte.florian@gmail.com>
2016-10-22 18:42:24 +0200

    Add new pages contest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: [:home,:team, :mention]
   def home
   end
   def mention
   end
   def team
+  end
+  def contest
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'pages/mention'
   get 'pages/team'
+  get 'pages/contest'
 
   devise_for :users
   root to: "pages#home"
