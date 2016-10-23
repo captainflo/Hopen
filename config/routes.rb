6190ec027fd58ddaf214878a64d12a5e4e6aee26
florian lahitte <lahitte.florian@gmail.com>
2016-10-23 10:01:42 +0200

    Add new style from pages_contest version4

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   get 'pages/contest'
 
   devise_for :users
+  devise_scope :user do
+  authenticated :user do
+    root 'pages#contest', as: :authenticated_root
+  end
+  unauthenticated do
+    root 'pages#home', as: :unauthenticated_root
+  end
+end
+
   root to: "pages#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
