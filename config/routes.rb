0e9ea862e7088c2ba1a5cf5c1f666126e7da25dc
florian lahitte <lahitte.florian@gmail.com>
2017-02-24 15:40:53 +0100

    fix photo facebook v4

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
   get 'pages/contest'
   get 'pages/db' => 'pages#index', as: :users
 
-
+  devise_scope :user do
+  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # user go directly to page contest after connect
+  # authenticated :user do
+  #   root 'pages#contest', as: :authenticated_root
+  # end
+  unauthenticated do
+    root 'pages#home', as: :unauthenticated_root
+  end
+end
 
   root to: "pages#home"
 
