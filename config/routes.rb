6fa1b1608cae6fb25d19b91373d00424c32f3a34
florian lahitte <lahitte.florian@gmail.com>
2017-02-24 15:32:31 +0100

    fix photo facebook v3

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,7 @@ Rails.application.routes.draw do
   get 'pages/contest'
   get 'pages/db' => 'pages#index', as: :users
 
-  devise_scope :user do
-  # user go directly to page contest after connect
-  # authenticated :user do
-  #   root 'pages#contest', as: :authenticated_root
-  # end
-  unauthenticated do
-    root 'pages#home', as: :unauthenticated_root
-  end
-end
+
 
   root to: "pages#home"
 
