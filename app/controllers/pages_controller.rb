99ff61d118825b302aeecd8be6bd234d1bf7e514
florian lahitte <lahitte.florian@gmail.com>
2017-05-09 09:51:29 +0200

    new homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, except: [:home,:team, :mention]
+  before_action :authenticate_user!, except: [:home,:home2,:team, :mention]
   def home
+  end
+  def home2
   end
   def mention
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,11 @@ Rails.application.routes.draw do
   devise_for :users,controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'pages/mention'
   get 'pages/team'
-  get 'pages/contest'
-  get 'pages/calendar'
   get 'pages/db' => 'pages#index', as: :users
 
-  devise_scope :user do
-  # user go directly to page contest after connect
-  authenticated :user do
-    root 'pages#calendar', as: :authenticated_root
-  end
-  unauthenticated do
-    root 'pages#home', as: :unauthenticated_root
-  end
-end
 
-  root to: "pages#home"
+
+  root to: "pages#home2"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
